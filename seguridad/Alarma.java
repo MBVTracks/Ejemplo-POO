d8ae17b52e3d80fbe8e63aed47462f43fc77ecfb
Mig Bar <miguel037.mb@gmail.com.com>
2021-03-01 17:40:38 +0100

    ejemplo examen POO

--- seguridad/Alarma.java
+++ seguridad/Alarma.java
@@ -1,4 +1,12 @@
 package clasesyobjetos.seguridad;
+import java.util.*;
+import java.util.ArrayList;
 
 public class Alarma {
+    TipoAlerta alerta;
+    List<Integer>sensor=new ArrayList<>();
+
+    public Alarma(TipoAlerta alerta) {
+        this.alerta = alerta;
+    }
 }
--- seguridad/Sensor.java
+++ seguridad/Sensor.java
@@ -1,4 +1,10 @@
 package clasesyobjetos.seguridad;
 
 public class Sensor {
+    TipoAlerta tipo;
+    boolean activado;
+
+    public Sensor(TipoAlerta tipo) {
+        this.tipo = tipo;
+    }
 }
